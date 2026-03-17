475f94a17caf631539ed66a863062a7fae8f1bb9
Brian Battauz <brian.nbattauz@gmail.com>
2026-03-17 12:53:29 -0300

    fix: correccion de estilos del check

--- src/components/ProductoModal.jsx
+++ src/components/ProductoModal.jsx
@@ -237,10 +237,10 @@ const ProductoModal = ({ show, onClose, onSave, editingProduct, categorias }) =>
                           checked={state.enOferta}
                           onChange={(e) => setState({ ...state, enOferta: e.target.checked })}
                         />
-                        <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-rose-500"></div>
+                        <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-brand-primary"></div>
                       </div>
                       <span className="text-[10px] font-black text-brand-secondary uppercase tracking-widest flex items-center gap-2">
-                        <FiTag className={state.enOferta ? "text-rose-500" : ""} />
+                        <FiTag className={state.enOferta ? "text-brand-primary" : ""} />
                         Prenda en Oferta (Sale)
                       </span>
                     </label>
@@ -258,7 +258,7 @@ const ProductoModal = ({ show, onClose, onSave, editingProduct, categorias }) =>
                           onChange={(e) => setState({ ...state, porcentajeDescuento: e.target.value })}
                           required={state.enOferta}
                         />
-                        <span className="text-xs font-black text-rose-500">% OFF</span>
+                        <span className="text-xs font-black text-brand-dark">% OFF</span>
                       </div>
                     )}
                   </div>
